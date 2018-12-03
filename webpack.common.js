72e34e8c3a744c20788c173ec0364700f39359ab
DESKTOP-PPFD0VC\admin <zhangx_k@126.com>
2018-12-03 23:56:19 +0800

    multi entry and multi output

--- webpack.common.js
+++ webpack.common.js
@@ -3,10 +3,13 @@ const webpack = require('webpack');  // 这个插件不需要安装，是基于w
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // 引入HtmlWebpackPlugin插件
 
 module.exports = {
-    entry: path.join(__dirname, "/src/index.js"), // 入口文件
+    entry: {
+        index: path.join(__dirname, "/src/index.js"),
+        two: path.join(__dirname, "/src/two.js")
+    }, 
     output: {
         path: path.join( __dirname, "/dist"), //打包后的文件存放的地方
-        filename: "bundle.js" //打包后输出文件的文件名
+        filename: "[name].js" //打包后输出文件的文件名
     },
     module: {
         rules: [
